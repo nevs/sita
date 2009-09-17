a7c881efd49f7deb5d1f48e540abad7ddf21593e
Sven Klemm <sven@c3d2.de>
2009-09-17 20:12:13 +0200

    make analyze.rb process multiple files

--- analyze.rb
+++ analyze.rb
@@ -6,49 +6,57 @@ require 'open3'
 require 'postgres'
 require 'rexml/document'
 
-conn = PGconn.open('port'=>'5433','host'=>'localhost')
 
-sql = File.new( ARGV[0] || "test.sql" ).read
-
-# parse sql statements of file
-xml = conn.exec("SELECT dump_sql_parse_tree($1);", sql )[0][0]
-
-# run sql file
-conn.exec( sql ) 
-
-# puts xml
-
-
-xml = REXML::Document.new( xml, {:ignore_whitespace_nodes=>:all} )
-
-xml.each_element('//CreateFunctionStmt') do | function |
-
-  func_name = []
-  arguments = []
+def analyse_file( filename )
+  sql = File.new( filename ).read
   
-  function.each_element('funcname/List/String/value') do | name |
-    func_name << name.text
-  end
+  # parse sql statements of file
+  xml = @conn.exec("SELECT dump_sql_parse_tree($1);", sql )[0][0]
   
-  function.each_element('parameters/List/FunctionParameter/argType/TypeName/names/List') do | arg |
-    names = []
-    arg.each_element('String/value') do | name |
-      names << name.text
+  # run sql file
+  @conn.exec( sql )
+  
+  # puts xml
+  
+  
+  xml = REXML::Document.new( xml, {:ignore_whitespace_nodes=>:all} )
+  
+  xml.each_element('//CreateFunctionStmt') do | function |
+  
+    func_name = []
+    arguments = []
+    
+    function.each_element('funcname/List/String/value') do | name |
+      func_name << name.text
     end
-    arguments << names.join('.')
+    
+    function.each_element('parameters/List/FunctionParameter/argType/TypeName/names/List') do | arg |
+      names = []
+      arg.each_element('String/value') do | name |
+        names << name.text
+      end
+      arguments << names.join('.')
+    end
+    
+    ast = @conn.exec("SELECT dump_plpgsql_function(#{@conn.quote("#{func_name.join('.')}(#{arguments.join(',')})")}::regprocedure);")[0][0]
+    
+    stdin, stdout, stderr = Open3.popen3("xsltproc xslt/plpgsql_function.xsl -")
+    stdin.write( ast )
+    stdin.close
+    
+    transformed_ast = stdout.read
+  #  puts transformed_ast
+    
+    function = Sita::Function.new( transformed_ast )
+    
+    Sita::Tests::SQL_Injection.new( function ).run
+  
   end
-  
-  ast = conn.exec("SELECT dump_plpgsql_function(#{conn.quote("#{func_name.join('.')}(#{arguments.join(',')})")}::regprocedure);")[0][0]
-  
-  stdin, stdout, stderr = Open3.popen3("xsltproc xslt/plpgsql_function.xsl -")
-  stdin.write( ast )
-  stdin.close
-  
-  transformed_ast = stdout.read
-  
-  function = Sita::Function.new( transformed_ast )
-  
-  Sita::Tests::SQL_Injection.new( function ).run
+end
 
+@conn = PGconn.open('port'=>'5433','host'=>'localhost')
+
+ARGV.each do | filename |
+  analyse_file( filename )
 end
 
